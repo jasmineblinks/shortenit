5601168988459d206997e1ad17df0f4de0e3f2dd
faith pueneh <puenehfaith@gmail.com>
2021-05-09 01:48:39 +0100

    style:updated the header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,16 +1,21 @@
 import React from "react";
-import classes from './header.module.css';
+import classes from "./header.module.css";
 
-const Header =()=>{
-    return (
-        <div className={classes.container}>
-            <section className={classes.mobile}>
-
-            </section>
-            <section className={classes.desktop}>
-                
-            </section>
-        </div>
-    )
-}
-export default Header
+const Header = () => {
+  return (
+    <div className={classes.container}>
+      <section className={classes.mobile}>
+        <header>
+            <div></div>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </header>
+      </section>
+      <section className={classes.desktop}></section>
+    </div>
+  );
+};
+export default Header;
