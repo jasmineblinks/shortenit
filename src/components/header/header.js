eef20113802f46d76f7a2d4d8d338c1cecbb8b1b
faith pueneh <puenehfaith@gmail.com>
2021-05-09 01:45:23 +0100

    feat: created the header component

---
+++ src/components/header/header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import classes from './header.module.css';
+
+const Header =()=>{
+    return (
+        <div className={classes.container}>
+            <section className={classes.mobile}>
+
+            </section>
+            <section className={classes.desktop}>
+                
+            </section>
+        </div>
+    )
+}
+export default Header
