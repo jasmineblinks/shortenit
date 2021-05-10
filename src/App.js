ebf16def263208f19717e675dd4f82df76316bcc
jasmineblinks <puenehfaith@gmail.com>
2021-05-10 17:58:56 +0100

    fix: updated the app and header component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <section className="container">
+        <h2 className="mainheading">Shorten your link </h2>
+        <p className="mainText">
+          Build your brand’s recognition and get detailed insights on how your
+          links are performing.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </section>
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer">
+        Learn React
+      </a>
     </div>
   );
 }
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,33 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./header.module.css";
-
+import { FaTimes, FaAlignRight } from "react-icons/fa";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className={classes.container}>
       <section className={classes.mobile}>
-        <header>
-            <section className={classes.logo}>
-                <h2>Jasmyne</h2>
-            </section>
-            <ul>
+        <header className={classes.header}>
+          <section className={classes.logo}>
+            <h2>JASMYNE</h2>
+          </section>
+
+          {isOpen !== true ? (
+            <FaAlignRight
+              size="35px"
+              style={{
+                color: "#ffffff",
+
+                marginRight: "25px",
+              }}
+              onClick={() => setIsOpen(true)}></FaAlignRight>
+          ) : (
+            <FaTimes
+              size="30px"
+              style={{
+                color: "#ffffff",
+
+                marginRight: "25px",
+              }}
+              onClick={() => setIsOpen(false)}></FaTimes>
+          )}
+          {isOpen && (
+            <section className={classes.menu}>
+              <ul className={classes.listItem}>
                 <li>Home</li>
                 <li>About</li>
                 <li>Contact</li>
-            </ul>
+              </ul>
+            </section>
+          )}
         </header>
       </section>
       <section className={classes.desktop}>
-      <section className={classes.logo}>
-                <h2>Jasmyne</h2>
-            </section>
-            <ul className={classes.list_head}>
-                <li>Home</li>
-                <li>About</li>
-                <li>Contact</li>
-            </ul>
-            <button>Sign Up</button>
-            <button>Login</button>
+        <header className={classes.header}>
+          <section className={classes.logo}>
+            <h2>JASMYNE</h2>
+          </section>
+          <ul className={classes.listItem}>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+            <button className={classes.btnOrange}>Sign Up</button>
+            <button className={classes.btnWhite}>Login</button>
+          </ul>
+        </header>
       </section>
     </div>
   );
