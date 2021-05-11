de8bc3e50894d7e15665c95133a6b95dfc6237cc
jasmineblinks <puenehfaith@gmail.com>
2021-05-11 02:33:37 +0100

    feat:updated the app component

--- src/App.js
+++ src/App.js
@@ -30,22 +30,24 @@ function App() {
             <input placeholder="Shorten a link here" />
             <button className="btnShort">Shorten It</button>
           </section>
-          <section className="secondText">
-            <h2>Who we are?</h2>
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has
-              survived not only five centuries, but also the leap into
-              electronic typesetting, remaining essentially unchanged. It was
-              popularised in the 1960s with the release of Letraset sheets
-              containing Lorem Ipsum passages, and more recently with desktop
-              publishing.
-            </p>
-          </section>
-          <section>
-            <img src={dev} className="App-dev" alt="logo" />
+          <section className="card">
+            <section className="secondText">
+              <h2>Who we are?</h2>
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book. It has
+                survived not only five centuries, but also the leap into
+                electronic typesetting, remaining essentially unchanged. It was
+                popularised in the 1960s with the release of Letraset sheets
+                containing Lorem Ipsum passages, and more recently with desktop
+                publishing.
+              </p>
+            </section>
+            <section>
+              <img src={dev} className="App-dev" alt="logo" />
+            </section>
           </section>
         </section>
       </section>
